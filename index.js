31dd552cb428a1c8d9105d0d8bf9705dfda3404e
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-10-12 13:36:25 +0200

    player movement

--- index.js
+++ index.js
@@ -8,16 +8,50 @@ c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
 // Background
-const img = new Image();
-img.src = './images/PokemonStyleMap.png';
+const backgroundImg = new Image();
+backgroundImg.src = './images/PokemonStyleMap.png';
 
 // Player
 const playerImage = new Image();
 playerImage.src = './images/playerDown.png';
 
-// Load images
-img.onload = () => {
-    c.drawImage(img, -910, -600);
+class Sprite {
+    constructor({position, image}) {
+        this.position = position;
+        this.image = image
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
+}
+
+const background = new Sprite({
+    position: {
+        x: -910,
+        y: -600
+    },
+    image: backgroundImg
+});
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    },
+}
+
+function animate() {
+    window.requestAnimationFrame(animate);
+    background.draw();
     c.drawImage(
         playerImage, 
         0, 
@@ -27,5 +61,54 @@ img.onload = () => {
         canvas.width / 2 - (playerImage.width / 4) / 2, 
         canvas.height / 2 - playerImage.height / 2,
         playerImage.width / 4,
-        playerImage.height)
+        playerImage.height
+    );
+
+    if(keys.w.pressed && lastKey === 'w') background.position.y += 2;
+    else if(keys.a.pressed && lastKey === 'a') background.position.x += 2;
+    else if(keys.s.pressed && lastKey === 's') background.position.y -= 2;
+    else if(keys.d.pressed && lastKey === 'd') background.position.x -= 2;
+
 }
+
+animate();
+
+// Movement
+let lastKey;
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w': 
+            keys.w.pressed = true;
+            lastKey = 'w';
+        break
+        case 'a': 
+             keys.a.pressed = true;
+             lastKey = 'a';
+        break
+        case 's': 
+            keys.s.pressed = true;
+            lastKey = 's';
+        break
+        case 'd': 
+            keys.d.pressed = true;
+            lastKey = 'd';
+        break
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w': 
+            keys.w.pressed = false;
+        break
+        case 'a': 
+             keys.a.pressed = false;
+        break
+        case 's': 
+            keys.s.pressed = false;
+        break
+        case 'd': 
+            keys.d.pressed = false;
+        break
+    }
+});
