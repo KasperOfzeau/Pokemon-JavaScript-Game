222f9585c3e34aec04e0c65e898e2afd8a4597cb
Kasper Beljaars <kasperbeljaars@hotmail.com>
2022-10-12 11:51:34 +0200

    player creation

--- index.js
+++ index.js
@@ -7,8 +7,25 @@ canvas.height = 576;
 c.fillStyle = 'white';
 c.fillRect(0, 0, canvas.width, canvas.height);
 
+// Background
 const img = new Image();
 img.src = './images/PokemonStyleMap.png';
+
+// Player
+const playerImage = new Image();
+playerImage.src = './images/playerDown.png';
+
+// Load images
 img.onload = () => {
-    c.drawImage(img, -1400, -800)
+    c.drawImage(img, -910, -600);
+    c.drawImage(
+        playerImage, 
+        0, 
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2, 
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height)
 }
